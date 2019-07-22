afcee69e242dc9a0243a72337eb5029f87521e03
Denis Cuenca Brex <Deniscbrex.dev@gmail.com>
2019-07-21 22:43:11 -0300

    add my skills section and some js

---
+++ main.js
@@ -0,0 +1,10 @@
+function mouseOver() {
+    document.getElementById("btn").style.background = "#4e94ec"
+    document.getElementById("btn").style.color = "white"
+    document.getElementById("btn").style.borderColor = "#4e94ec"
+}
+
+function mouseOut() {
+    document.getElementById("btn").style.background = "white"
+    document.getElementById("btn").style.color = "#4e94ec"
+}
