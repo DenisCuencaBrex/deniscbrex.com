2a3aa8255afae024262aafec3bd086746335bb1b
Denis Cuenca Brex <Deniscbrex.dev@gmail.com>
2019-08-26 06:15:16 -0300

    fixed phone resolution

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 function dropItems() {
     links = document.getElementById("burguer-links");
+    burguer = document.getElementById("hamburguer");
     if (links.style.display === "block") {
         links.style.display = "none";
+        burguer.style.color = "#4e94ec";
     } else {
         links.style.display = "block";
+        burguer.style.color = "black";
     }
 }
