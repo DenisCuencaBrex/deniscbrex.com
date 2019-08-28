60f7f5efe823250b39fd87fba7dc587d9362c16d
Denis Cuenca Brex <Deniscbrex.dev@gmail.com>
2019-08-27 21:30:02 -0300

    changed phone hamburguer menu

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 function dropItems() {
     links = document.getElementById("burguer-links");
     burguer = document.getElementById("hamburguer");
+    exit = document.getElementById("exit");
     if (links.style.display === "block") {
         links.style.display = "none";
-        burguer.style.color = "#4e94ec";
+        burguer.style.display = "block";
+        exit.style.display = "none";
     } else {
         links.style.display = "block";
-        burguer.style.color = "black";
+        burguer.style.display = "none";
+        exit.style.display = "block";
     }
 }
